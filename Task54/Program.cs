2af0deeb34a3ef2759571fb00c6554e6217258c6
Sarmaon <patratiyaa@gmail.com>
2023-02-13 18:14:22 +0800

    fiks

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,34 +1,65 @@
 ﻿//Задача 54: Задайте двумерный массив. Напишите программу,
 //которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
-Console.WriteLine("Введите первое значение ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите первое значение ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-int[,] array = newArraw[m, n];
-InitArray(array);
-WriteArray(array);
-
-void ArrayM(int[,] array)
+int[,] CreateArray(int horiz, int vert, int min, int max)
 {
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
+    int[,] array = new int[horiz, vert];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int k = 0; k < array.GetLength(1); k++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (array[i,k] < array[i, k+1])
-        }
-        {
-                int temp = array[i, k+1];
-                array[i, k+1] = array[i, k];
-                array[i, k] = temp;
+            array[i, j] = rnd.Next(min, max + 1);
         }
     }
+    return array;
 }
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j < array.GetLength(1) - 1) Console.Write($" {array[i, j],5},");
+            else Console.Write($"{array[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
 }
+
+int[,] HorizSortArray(int[,] Array)
+{
+    int tempMax;
+
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < Array.GetLength(1) - 1 - j; k++)
+            {
+                if (Array[i, k] < Array[i, k + 1])
+                {
+                    tempMax = Array[i, k];
+                    Array[i, k] = Array[i, k + 1];
+                    Array[i, k + 1] = tempMax;
+                }
+            }
+        }
+    }
+    return Array;
+}
+
+
+int[,] array = CreateArray(4, 5, -10, 10);
+PrintArray(array);
+
+Console.WriteLine("Отсортированно по убыванию ");
+
+int[,] HorizSort = HorizSortArray(array);
+PrintArray(HorizSort);
 
 int Numbers(string input)
 {
